68ed976595005618e89c49db155d1ff981e68208
naszly <naszaly.k@gmail.com>
2022-08-29 01:31:44 +0200

    set cursor shape

--- engine/src/core/input.h
+++ engine/src/core/input.h
@@ -9,7 +9,21 @@
 enum class CursorMode {
     Normal = GLFW_CURSOR_NORMAL,
     Hidden = GLFW_CURSOR_HIDDEN,
-    Disabled = GLFW_CURSOR_DISABLED
+    Disabled = GLFW_CURSOR_DISABLED,
+    Captured = GLFW_CURSOR_CAPTURED
+};
+
+enum class CursorShape {
+    Arrow = GLFW_ARROW_CURSOR,
+    IBeam = GLFW_IBEAM_CURSOR,
+    Crosshair = GLFW_CROSSHAIR_CURSOR,
+    Hand = GLFW_POINTING_HAND_CURSOR,
+    ResizeEastWest = GLFW_RESIZE_EW_CURSOR,
+    ResizeNorthSouth = GLFW_RESIZE_NS_CURSOR,
+    ResizeNorthEastSouthWest = GLFW_RESIZE_NESW_CURSOR,
+    ResizeNorthWestSouthEast = GLFW_RESIZE_NWSE_CURSOR,
+    ResizeAll = GLFW_RESIZE_ALL_CURSOR,
+    NotAllowed = GLFW_NOT_ALLOWED_CURSOR
 };
 
 class Input {
@@ -58,8 +72,20 @@ public:
         glfwSetInputMode(glfwWindow, GLFW_CURSOR, static_cast<int>(mode));
     }
 
+    void setCursorShape(CursorShape shape) {
+        if (shape != cursorShape) {
+            GLFWcursor *cursor = glfwCreateStandardCursor(static_cast<int>(shape));
+            glfwSetCursor(glfwWindow, cursor);
+            glfwDestroyCursor(glfwCursor);
+            glfwCursor = cursor;
+            cursorShape = shape;
+        }
+    }
+
 private:
     GLFWwindow *glfwWindow{nullptr};
+    GLFWcursor *glfwCursor{nullptr};
+    CursorShape cursorShape{CursorShape::Arrow};
 };
 
 #endif //ENGINE_SRC_CORE_INPUT_H
